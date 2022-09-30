940d99b61f3a0e677c5b116a28d435d0bf8591bd
Ankush Soni <ankushsoni11@gmail.com>
2022-09-30 23:16:16 +0530

    FIXING

--- movie/src/main/resources/schema.sql
+++ movie/src/main/resources/schema.sql
@@ -3,5 +3,6 @@ CREATE TABLE `movie` (
   `movie_id` bigint NOT NULL,
   `movie_name` varchar(255) NOT NULL,
   `city_id` bigint(255) NOT NULL,
-  `theater_id` bigint(255) NOT NULL
+  `theater_id` bigint(255) NOT NULL,
+  PRIMARY KEY (`movie_id`)
 );
--- show/src/main/java/com/movietime/show/entity/Show.java
+++ show/src/main/java/com/movietime/show/entity/Show.java
@@ -1,7 +1,6 @@
 package com.movietime.show.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -10,6 +9,9 @@ import java.util.Date;
 @Table(name = "show")
 @Getter
 @Setter
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class Show {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
